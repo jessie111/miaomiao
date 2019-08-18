d3f28b6a4976771ffb9edb815fa614bdf58da718
jessie <269506360@qq.com>
2019-08-18 18:16:19 +0800

    add createComponents

---
+++ src/components/CiMenu/index.vue
@@ -0,0 +1,23 @@
+<template>
+        <div class="cinema_menu">
+            <div class="city_switch">
+                全城 <i class="iconfont icon-lower-triangle"></i>
+            </div>
+            <div class="brand_swtich">
+                品牌 <i class="iconfont icon-lower-triangle"></i>
+            </div>
+            <div class="feature_switch">
+                特色 <i class="iconfont icon-lower-triangle"></i>
+            </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        name: "CiMenu"
+    }
+</script>
+
+<style scoped>
+ .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+</style>
---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="main">
+        <Header title="喵喵影院"></Header>
+        <div id="content">
+            <CiMenu></CiMenu>
+            <CiList></CiList>
+        </div>
+        <TabBar></TabBar>
+    </div>
+</template>
+
+<script>
+    import Header from "@/components/Header"
+    import TabBar from "@/components/TabBar"
+    import CiList from "@/components/CiList"
+    import CiMenu from "@/components/CiMenu"
+
+    export default {
+        name: "Cinema",
+        components:{
+            Header,
+            TabBar,
+            CiList,
+            CiMenu
+        }
+    }
+</script>
+
+<style scoped>
+
+
+</style>
